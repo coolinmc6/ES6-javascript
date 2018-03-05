82520eef9ff11f0dc8d79406abe21c5e041ccbfe
Colin McNamara <coolinmc6@gmail.com>
2018-03-05 11:32:33 -0500

    Classes lectures

--- 15-Classes/classes.js
+++ 15-Classes/classes.js
@@ -25,3 +25,33 @@ car;
 
 car.honk();
 car.drive();
+
+
+// ES6
+class Car {
+  constructor({title}) {
+    this.title = title
+  }
+	drive() {
+   return 'vroom' 
+  }
+
+};
+
+
+class Toyota extends Car {
+  constructor(options) {
+    super(options);		// Car.constructor()
+    this.color = options.color;
+  }
+  
+  honk() {
+    return 'beep';
+  };
+}
+
+const toyota = new Toyota({color: 'red', title: 'CMs Car'});
+toyota.honk()
+
+
+toyota.drive();
